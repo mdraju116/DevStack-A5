7e72474a3794d02a9eff72bf0fbc327eade4cb5d
Raju204116 <raju204116@gmail.com>
2026-09-12 22:45:48 +0600

    banner-completed

--- DevStack/src/App.tsx
+++ DevStack/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './components/Banner'
 import Nav from './components/Nav'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      
      <Nav />
+     <Banner/>
      
     </>
   )
--- DevStack/src/components/Banner.tsx
+++ DevStack/src/components/Banner.tsx
@@ -1,9 +1,45 @@
+import heroIcon from "../assets/banner-stack.png"
 
 export interface BannerProps {
     prop: string
 }
 
-export default function Banner({ prop }: BannerProps) {
-    
-    return 
+export default function Banner() {
+
+    return (
+        <div className="flex container mx-auto mt-12 px-6 md:px-10 lg:px-16 py-4 items-center justify-between">
+
+            {/* left side */}
+            <div className="flex flex-col gap-6">
+                <h1 className="font-bold font-inter text-4xl">
+                    Build Your Ideal <br />
+                    <span className="bg-linear-to-r from-[#ff2a00] to-purple-700 bg-clip-text text-transparent">
+                        Development Stack
+                    </span>
+                </h1>
+
+                <p className="text-[#475569] font-400">
+                    Explore frontend, backend, database, and tooling options,<br />
+                    compare them side by side, and put together the stack that fits your <br />
+                    next project.
+                </p>
+
+                <div className="flex gap-4">
+                    <button className="bg-linear-to-r from-[#f66936] to-[#ed4c92] text-white px-6 py-3 rounded-xl">
+                        Explore Technologies
+                    </button>
+
+                    <button className="bg-white text-black border border-gray-200 px-10 py-3 rounded-xl">
+                        Learn More
+                    </button>
+                </div>
+            </div>
+
+            {/* right side */}
+            <div>
+                <img src={heroIcon} alt="" />
+            </div>
+
+        </div>
+    )
 }
